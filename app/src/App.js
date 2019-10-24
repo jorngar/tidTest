21ae5b19ec88de2541858dc7f5cdd43055e05a64
GARAY FOGOLIN Jorge nelson <JorgeNelson.GARAYFOGOLIN@cegedim.com>
2019-10-25 00:34:37 +0200

    Ejercicio 2, I would change the whole implementation to use redux or graphql but I went for a quick change, test is failing I will fix it before final commit

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,8 @@ const domain = 'http://localhost:3001'
 
 class App extends PureComponent {
   state = {
-    teams: []
+    teams: [],
+    players: []
   }
 
   componentDidMount() {
@@ -17,37 +18,35 @@ class App extends PureComponent {
       .then(teams => {
         this.setState({ teams })
       });
+    fetch(`${domain}/players`)
+      .then(response => {
+        return response.json();
+      })
+      .then(players => {
+        this.setState({ players })
+      });
   }
 
   render() {
-    const { teams } = this.state
-
+    const { players } = this.state
+    console.log(players)
     return <div className="App">
-      <header className="App-heading App-flex">
-        <h2>Bienvenido a la prueba de los equipos</h2>
-      </header>
-      <div className="App-teams App-flex">
-        {/* 
+      {/*
           TODO ejercicio 2
-          Debes obtener los players en lugar de los equipos y pintar su nombre. 
+          Debes obtener los players en lugar de los equipos y pintar su nombre.
           Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
-          y una lista con sus nombres. 
+          y una lista con sus nombres.
           ** Los comentarios de los ejercicios no los borres.
         */}
-        <h3>Los equipos:</h3>
-        <ul>
-          {/* 
+      <h3>Los jugadores:</h3>
+      <ul>
+        {/*
             TODO ejercicio 3
             Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
             Guiate por las imágenes.
            */}
-          {teams.map(team => <li key={team.id}>{team.name}</li>)}
-        </ul>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={reactSvg}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
+        {players.map(player => <li>{player.name}</li>)}
+      </ul>
     </div>
   }
 }
