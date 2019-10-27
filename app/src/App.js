8fdfe8f2e5ce19de3f35c040a1b665eb22e83c11
GARAY FOGOLIN Jorge nelson <JorgeNelson.GARAYFOGOLIN@cegedim.com>
2019-10-27 20:47:23 +0100

    Ejercicio 3, I used material ui library since it fitted nicely with what we needed for the design, I have also added prop-types for correct prop drilling between child component and parent component I would refactor shild component to a functional componet it would probably make a better choice for it

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,20 @@
-import reactSvg from './react.svg'
 import './App.css'
+import React, {PureComponent} from 'react'
+import {Grid} from '@material-ui/core'
+import PlayerCard from "./PlayerCard";
 
-import React, { PureComponent } from 'react'
 const domain = 'http://localhost:3001'
 
+//TODO: reduce class complexity
 class App extends PureComponent {
-  state = {
-    teams: [],
-    players: []
+  constructor() {
+    super();
+    this.state = {
+      teams: [],
+      players: [],
+      avatarSize: 280,
+      cardStyle: 'regularCard'
+    }
   }
 
   componentDidMount() {
@@ -16,37 +23,44 @@ class App extends PureComponent {
         return response.json();
       })
       .then(teams => {
-        this.setState({ teams })
+        this.setState({teams})
       });
     fetch(`${domain}/players`)
       .then(response => {
         return response.json();
       })
       .then(players => {
-        this.setState({ players })
+        this.setState({players})
       });
   }
 
   render() {
-    const { players } = this.state
-    console.log(players)
+    const {players, teams} = this.state
+    console.log(teams)
     return <div className="App">
-      {/*
-          TODO ejercicio 2
-          Debes obtener los players en lugar de los equipos y pintar su nombre.
-          Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
-          y una lista con sus nombres.
-          ** Los comentarios de los ejercicios no los borres.
-        */}
-      <h3>Los jugadores:</h3>
-      <ul>
-        {/*
+      <div className="App-teams App-flex"> {
+        /*
+                  TODO ejercicio 2
+                  Debes obtener los players en lugar de los equipos y pintar su nombre.
+                  Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
+                  y una lista con sus nombres.
+                  ** Los comentarios de los ejercicios no los borres.
+                */
+      }
+        <h3>Jugadores:</h3>
+        <Grid container justify="center" alignItems="center">
+          {/*
             TODO ejercicio 3
             Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
             Guiate por las imágenes.
            */}
-        {players.map(player => <li>{player.name}</li>)}
-      </ul>
+          {players.map(player =>
+            //TODO move styles into a separate js file and export this class using withStyles or similar or just to css file
+            <Grid item xs={12} sm={6} lg={4}>
+              <PlayerCard player={player} teams={teams}/>
+            </Grid>)}
+        </Grid>
+      </div>
     </div>
   }
 }
---
+++ app/src/PlayerCard.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import './App.css'
+import {Avatar, Card, Typography} from '@material-ui/core'
+import PropTypes from 'prop-types';
+
+const domain = 'http://localhost:3001'
+
+class PlayerCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avatarSize: 280,
+      cardStyle: 'regularCard',
+      shieldStyle: 'regularShield'
+    }
+    this.responsiveStyle = this.responsiveStyle.bind(this);
+  }
+
+  componentDidMount() {
+    this.responsiveStyle();
+    window.addEventListener('resize', this.responsiveStyle)
+  }
+
+  responsiveStyle() {
+    if (window.innerWidth < 601) {
+      this.setState({avatarSize: 60, cardStyle: 'smallCard', shieldStyle:'smallShield'})
+    }
+    else {
+      this.setState({avatarSize: 280, cardStyle: 'regularCard', shieldStyle:'regularShield'})
+    }
+  }
+
+  shieldHandler(player, teams) {
+    let shield = '';
+    teams.map(team => {
+      if (player.teamId === team.id) {
+        shield = team.shield.replace('//', '')
+      }
+    })
+    return shield;
+  }
+
+  render() {
+    const {avatarSize, cardStyle, shieldStyle} = this.state
+    const {player, teams} = this.props;
+    return (
+      <Card className={cardStyle}>
+        {shieldStyle === 'regularShield'?
+        <img src={'https://' + this.shieldHandler(player, teams)} className={shieldStyle}/>:null}
+        <Avatar alt={player.name} src={player.img ? player.img : null}
+                style={{margin: 5, width: avatarSize, height: avatarSize}}/>
+        <div style={{flexDirection:'column'}}>
+          <Typography style={{display: 'inline-block', paddingLeft:'2%', minWidth:300}} noWrap>
+            {player.name}
+            <Typography style={{fontWeight: 'bold', display: 'inline-block', paddingLeft:'2%'}}>
+              {player.position}
+            </Typography>
+          </Typography>
+        <Typography style={{paddingLeft:'2%', fontSize:14}}>{teams.map(team => {
+          if (player.teamId === team.id) {
+            return team.name
+          }
+        })}</Typography>
+        </div>
+        {shieldStyle === 'smallShield'?
+          <img src={'https://' + this.shieldHandler(player, teams)} className={shieldStyle}/>:null}
+      </Card>
+    )
+  }
+}
+
+PlayerCard.propTypes = {
+  player: PropTypes.object.isRequired,
+  teams: PropTypes.object.isRequired
+}
+
+export default PlayerCard;
